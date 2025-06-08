b6380bd2560155d3163754327d15c6e8aa4b1b8b
meghana1228 <kadiyam.meghana@gmail.com>
2025-06-08 17:57:01 +0530

    variables

--- 08-sample-program.sh
+++ 08-sample-program.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# check root access of the user
 userid=$(id -u)
 if [ $userid -eq 0 ]
 then 
@@ -6,4 +7,15 @@ echo " you are running with root access"
 else 
 echo " you are not running with root access"
 exit 1
+fi
+
+# check already installed or not
+
+dnf list installed mysql 
+if [ $? -eq 0 ]
+then
+echo " mysql is already installed "
+exit 1
+else
+echo " mysql is not installed so installing it"
 fi
