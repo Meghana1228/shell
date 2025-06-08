77106ac7095851ef444bcc65fce7f693303474e8
meghana1228 <kadiyam.meghana@gmail.com>
2025-06-08 19:10:48 +0530

    variables

--- swamy.sh
+++ swamy.sh
@@ -17,12 +17,28 @@ then
 echo "mysql is already installed: nothing to do"
 else
 echo "mysql is not installed: going to install"
-dnf install mysql
+dnf install mysql -y
 if [ $? -eq 0 ]
 then
 echo "mysql is successfully installed"
 else
 echo "mysql is installation is failed"
+exit 1
+fi
+fi
+# installing python3
+dnf list installed python3
+if [ $? -eq 0 ]
+then
+echo "python3 is already installed: nothing to do"
+else
+echo "python3 is not installed: going to install"
+dnf install python3 -y
+if [ $? -eq 0 ]
+then
+echo "python3 is successfully installed"
+else
+echo "python3 is installation is failed"
 exit 1
 fi
 fi
