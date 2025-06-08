d0ba57361579e2a65c7b9be9ec9a138b2fecd4b3
meghana1228 <kadiyam.meghana@gmail.com>
2025-06-08 19:07:08 +0530

    variables

--- swamy.sh
+++ swamy.sh
@@ -9,3 +9,20 @@ else
 echo "you are not having root access: exiting"
 exit 1
 fi
+
+#installing the mysql
+dnf list installed mysql
+if [ $? -eq 0 ]
+then
+echo "mysql is already installed: nothing to do"
+else
+echo "mysql is not installed: going to install"
+dnf install mysql
+if [ $? -eq 0 ]
+then
+echo "mysql is successfully installed"
+else
+echo "mysql is installation is failed"
+exit 1
+fi
+fi
