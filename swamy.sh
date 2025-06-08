f1e141abf8116e76e0bba0eb20c5665412dd64fd
meghana1228 <kadiyam.meghana@gmail.com>
2025-06-08 20:20:55 +0530

    variables

--- swamy.sh
+++ swamy.sh
@@ -22,21 +22,21 @@ fi
 validate(){
     if [ $1 -eq 0 ]
 then
-echo -e "$2 is $G successfully installed $N" &>>$logfile
+echo -e "$2 is $G successfully installed $N"&>>$logfile
 else
-echo -e "$2 installation is $Rfailed $N" &>>$logfile
+echo -e "$2 installation is $Rfailed $N"&>>$logfile
 exit 1
 fi
 }
 
 for package in ${packages[@]}
 do
-dnf list installed $package & >> $logfile
+dnf list installed $package &>>$logfile
 if [ $? -eq 0 ]
 then
-echo -e "$package is $G already installed: nothing to do $N" &>>$logfile
+echo -e "$package is $G already installed: nothing to do $N"&>>$logfile
 else
-echo -e "$package is not installed:  $G going to install$N" &>>$logfile
+echo -e "$package is not installed:  $G going to install$N"&>>$logfile
 dnf install $package -y
 validate $? "$package"
 fi
